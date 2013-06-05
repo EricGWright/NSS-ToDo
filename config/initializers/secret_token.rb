8fa9ff3d172fe44caa9b42b33fbd743e3f6644be
EricGWright <musicmarketer@gmail.com>
2013-06-05 11:15:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NSSTodo::Application.config.secret_token = 'a1d1d7ded2575bfb6d5ce33a128ae746ca41ea3071b099003372c51e84f44d86adb2d0c4650e7d55229e28497b44734ffbc98b49ba89f07f5df2368d93cc8060'
